1d2e281505db906e64a83b89999fd9bfc5b3cfbe
Lorenzo Gandini <64914278+Lorenzo-Gandini@users.noreply.github.com>
2020-10-16 16:22:11 +0200

    piccola deviazione

--- functions.js
+++ functions.js
@@ -21,20 +21,16 @@ function addTheText() {
   createDiv(toDoList.length);
 }
 
-
 //This function creates a new object and pushes it into the array TtoDoList
 function addItem(txt){
-  //Creates a new object with the text, the flags and the date
   const note = {
     text : txt.value,
     done : false,
     print : false, 
-    add : new Date()
   };
   //Adds the object at the end of the array
   toDoList.push(note);
 }
-
 
 //This function iterate the print function for each object into the toDoList array.
 function showTheText() {
@@ -45,33 +41,39 @@ function hideTheText() {
   toDoList.forEach(hide);
 }
 
-//Function that create a checkbox for each div
-function createCheckBox(index) {
-  var checkbox = document.createElement('input');
-  checkbox.type = "checkbox";
-  checkbox.id = "cb"+index;
-  return checkbox;
-}
-
 //This function print the text into a different div
-function print(group, index){
-  document.getElementById("div"+index).style.display = 'block';
-  toDoList[index].print = true;
+function print(index){
+  if(!(toDoList[index].print)){
+    document.getElementById("div"+index).style.display = 'block';
+    toDoList[index].print = true;
+  }
 }
 
-function hide(group, index){
-  document.getElementById("div"+index).style.display = 'none'; 
-  toDoList[index].print = false;
+function hide(index){
+  if(toDoList[index].print){
+    document.getElementById("div"+index).style.display = 'none'; 
+    toDoList[index].print = false;
+  }
 }
 
-function createDiv(group, index){
+//Function that creates a div for each text
+function createDiv(index){
   var my_string = toDoList[index].text;
   const div = document.createElement("div");
   div.id = "div"+index;
+  div.style.display = 'none';
   var textnode = document.createTextNode(my_string); 
   var checkbox = createCheckBox(index);
   div.classList.add("note");
   div.appendChild(checkbox);
   div.appendChild(textnode);
   document.getElementById("content").appendChild(div);
+}
+
+//Function that create a checkbox for each div
+function createCheckBox(index) {
+  var checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.id = "cb"+index;
+  return checkbox;
 }
